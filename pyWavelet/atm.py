b732da73aba1c07ffafef7a13a44010f4c96da19
Mike Brady <mike.brady@canada.ca>
2018-08-29 13:51:53 -0400

    ITP Take 2
    - Add super().__init__() to OIB child class
    - Add __str__ definition to OIB/AWI as example of overriding base class
    methods and using py3 f-strings
    - Constants in all-caps
    - Make use of asterisk-unpacking where possible (e.g. self.poly
    creation)
    - Use .value rather than [()] notation when extracting data from
    h5py.File
    - Avoid overriding builtin types (e.g. naming a variable "dict")
    - Simplify requirements.yml (add geopandas/descartes. should be
    OS-agnostic now)
    - Small formatting and grammar changes

--- pyWavelet/atm.py
+++ pyWavelet/atm.py
@@ -1,54 +1,55 @@
+import os
 import h5py
 import numpy as np
 import timefunc
 from datetime import datetime, timedelta, timezone
 from shapely.geometry import box
 
-_qc_pitch_max = 5 #Max ATM pitch in def
-_qc_roll_max = 5 #Max ATM roll in deg
+QC_PITCH_MAX = 5 #Max ATM pitch in def
+QC_ROLL_MAX = 5 #Max ATM roll in deg
 
 class atm:
-    def __init__(self, file_path, l_case = 'meta'):
+    def __init__(self, file_path, l_case='meta'):
         with h5py.File(file_path, 'r') as f:
-            self.time_packed = np.around(f['instrument_parameters']['time_hhmmss'][()],3)[[0,-1]] #This is a weird time format...
-            self.extent = np.hstack((np.mod((f['ancillary_data']['min_longitude'][()]+180),360)-180,
-                                     f['ancillary_data']['min_latitude'][()],
-                                     np.mod((f['ancillary_data']['max_longitude'][()]+180),360)-180,
-                                     f['ancillary_data']['max_latitude'][()])).ravel()
-            if (l_case=='full'):
-                self.elv = f['elevation'][()]
-                self.lat = f['latitude'][()]
-                self.lon = np.mod((f['longitude'][()]+180),360)-180
-                #self.elasped = f['instrument_parameters']['rel_time'][()]
-                self.pitch = f['instrument_parameters']['pitch'][()]
-                self.roll =f['instrument_parameters']['roll'][()]
-                self.time_packed = np.around(f['instrument_parameters']['time_hhmmss'][()],3) #This is a weird time format...
+            self.time_packed = np.around(f['instrument_parameters']['time_hhmmss'].value, 3)[[0, -1]] #This is a weird time format...
+            self.extent = np.hstack((np.mod((f['ancillary_data']['min_longitude'].value + 180), 360) - 180,
+                                     f['ancillary_data']['min_latitude'].value,
+                                     np.mod((f['ancillary_data']['max_longitude'].value + 180), 360) - 180,
+                                     f['ancillary_data']['max_latitude'].value)).ravel()
+            if (l_case == 'full'):
+                self.elv = f['elevation'].value
+                self.lat = f['latitude'].value
+                self.lon = np.mod((f['longitude'].value + 180), 360) - 180
+                #self.elapsed = f['instrument_parameters']['rel_time'].value
+                self.pitch = f['instrument_parameters']['pitch'].value
+                self.roll = f['instrument_parameters']['roll'].value
+                self.time_packed = np.around(f['instrument_parameters']['time_hhmmss'].value, 3) #This is a weird time format...
             
-        self.poly = box(self.extent[0], self.extent[1], self.extent[2], self.extent[3])        
-        self.file_name = file_path.split('\\')[-1]
-        self.file_path = file_path
+        self.poly = box(*self.extent)
+        self.file_path = os.path.abspath(file_path)
+        self.file_name = os.path.basename(file_path)        
         self.load_type = l_case
         
         #Sort out the file times
-        self.time_epoch = datetime.strptime(self.file_name [8:23], "%Y%m%d_%H%M%S").replace(tzinfo=timezone.utc).timestamp()
+        self.time_epoch = datetime.strptime(self.file_name[8:23], "%Y%m%d_%H%M%S").replace(tzinfo=timezone.utc).timestamp()
         self.calcutc()
         
         
     def as_dict(self):
-        if (self.load_type=='meta'):
+        if (self.load_type == 'meta'):
             return {'fname': self.file_name, 
                     'fpath': self.file_path, 
-                    'tstart':self.time_utc[0], 
-                    'tend':self.time_utc[-1],
+                    'tstart': self.time_utc[0], 
+                    'tend': self.time_utc[-1],
                     'poly': self.poly}
         
     def calcutc(self):
         hh_rem = np.mod(self.time_packed, 1e4)
-        hh = (self.time_packed-hh_rem)/1e4
-        mm = np.modf(hh_rem/1e2)[1]
+        hh = (self.time_packed - hh_rem) / 1e4
+        mm = np.modf(hh_rem / 1e2)[1]
         ss = np.modf(np.mod(hh_rem, 1e2))[1]
         ms_rem = np.around(np.modf(hh_rem)[0], 3)
-        gps_seconds = (hh*3600)+(mm*60)+ss+ms_rem
+        gps_seconds = (hh * 3600) + (mm * 60) + ss + ms_rem
         time_start = datetime.utcfromtimestamp(self.time_epoch)
-        gps_ts = time_start.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc).timestamp()+gps_seconds
+        gps_ts = time_start.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc).timestamp() + gps_seconds
         self.time_utc = timefunc.utcleap(gps_ts)
--- pyWavelet/picklayers.py
+++ pyWavelet/picklayers.py
@@ -1,48 +1,48 @@
 import numpy as np
 import pywt
 
-def picklayers(data,null_2_space,delta_fast_time_range, n_snow, ref_snow_layer = 1):
-    ref_scale_lin_m = 2*null_2_space
-    max_scale_lin = np.ceil(ref_scale_lin_m/delta_fast_time_range)
-    lin_scale_vect_pre = np.arange(2,max_scale_lin,1)
-    lin_scale_vect = lin_scale_vect_pre [np.mod(lin_scale_vect_pre,2)==1]
+def picklayers(data, null_2_space, delta_fast_time_range, n_snow, ref_snow_layer=1):
+    ref_scale_lin_m = 2 * null_2_space
+    max_scale_lin = np.ceil(ref_scale_lin_m / delta_fast_time_range)
+    lin_scale_vect_pre = np.arange(2, max_scale_lin, 1)
+    lin_scale_vect = lin_scale_vect_pre[np.mod(lin_scale_vect_pre, 2) == 1]
     
-    snow_layer_opl = ref_snow_layer*n_snow*2
-    ref_scale_log_m = 2*snow_layer_opl
-    max_scale_log = np.ceil(ref_scale_log_m/delta_fast_time_range)
-    log_scale_vect_pre = np.arange(2,max_scale_log,1)
-    log_scale_vect = log_scale_vect_pre [np.mod(log_scale_vect_pre,2)==1]
+    snow_layer_opl = ref_snow_layer * n_snow * 2
+    ref_scale_log_m = 2 * snow_layer_opl
+    max_scale_log = np.ceil(ref_scale_log_m / delta_fast_time_range)
+    log_scale_vect_pre = np.arange(2, max_scale_log, 1)
+    log_scale_vect = log_scale_vect_pre[np.mod(log_scale_vect_pre, 2) == 1]
     
-    lin_coefs = cwt(data, pywt.Wavelet('haar'),lin_scale_vect)
-    log_coefs = cwt(10*np.log10(data), pywt.Wavelet('haar'),log_scale_vect)
+    lin_coefs = cwt(data, pywt.Wavelet('haar'), lin_scale_vect)
+    log_coefs = cwt(10 * np.log10(data), pywt.Wavelet('haar'), log_scale_vect)
     
-    lin_coefs[:, 0:np.ceil(max_scale_lin/2).astype(int)] = 0
-    lin_coefs[:, -np.ceil(max_scale_lin/2).astype(int):-1] = 0
+    lin_coefs[:, 0:np.ceil(max_scale_lin / 2).astype(int)] = 0
+    lin_coefs[:, -np.ceil(max_scale_lin / 2).astype(int):-1] = 0
     
-    log_coefs[:, 0:np.ceil(max_scale_log/2).astype(int)] = 0
-    log_coefs[:, -np.ceil(max_scale_log/2).astype(int):-1] = 0
+    log_coefs[:, 0:np.ceil(max_scale_log / 2).astype(int)] = 0
+    log_coefs[:, -np.ceil(max_scale_log / 2).astype(int):-1] = 0
     
-    sum_log_coefs = np.sum(log_coefs,axis=0)/log_coefs.shape[0]
-    sum_lin_coefs = np.sum(lin_coefs,axis=0)/lin_coefs.shape[0]
+    sum_log_coefs = np.sum(log_coefs,axis=0) / log_coefs.shape[0]
+    sum_lin_coefs = np.sum(lin_coefs,axis=0) / lin_coefs.shape[0]
     
     locs_si = np.argmax(-sum_lin_coefs)
     locs_as = np.argmax(-sum_log_coefs)
 
-    return locs_as,locs_si
+    return locs_as, locs_si
 
-def cwt(data, wavelet, scales, precision = 10):
+def cwt(data, wavelet, scales, precision=10):
     out_coefs = np.zeros((np.size(scales), data.size))
     int_psi, x = pywt.integrate_wavelet(wavelet, precision=precision)
     step = x[1] - x[0]
     x_step = (x[-1] - x[0]) + 1
     
-    j_a = [np.arange(scale * x_step)/(scale*step) for scale in scales]
+    j_a = [np.arange(scale * x_step) / (scale * step) for scale in scales]
     j_m = [np.delete(j, np.where((j >= np.size(int_psi)))[0]) 
            for j in j_a if np.max(j) >= np.size(int_psi)]
     coef_a = [-np.sqrt(scales[i]) 
-              *np.diff(np.convolve(data, int_psi[x.astype(np.int)][::-1])) 
-              for (i,x) in enumerate(j_m)]
-    d_a = [(coef.size-data.size)/2 for coef in coef_a]
-    out_coefs = np.asarray([coef[int(np.floor((coef.size-data.size)/2 ))
-                                 :int(-np.ceil((coef.size-data.size)/2 ))] for coef in coef_a])
+               * np.diff(np.convolve(data, int_psi[x.astype(np.int)][::-1]))
+              for (i, x) in enumerate(j_m)]
+    d_a = [(coef.size - data.size) / 2 for coef in coef_a]
+    out_coefs = np.asarray([coef[int(np.floor((coef.size - data.size) / 2))
+                            :int(-np.ceil((coef.size - data.size) /2))] for coef in coef_a])
     return out_coefs
