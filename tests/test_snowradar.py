07a48bc5062fd2314255bae2855dcb663fddf2ba
Mike Brady <mike.brady@canada.ca>
2018-12-10 10:47:41 -0500

    Drop calcpulse.py
    Redundant now that the SnowRadar class has its own method for doing the
    same thing

--- tests/test_snowradar.py
+++ tests/test_snowradar.py
@@ -8,6 +8,7 @@ TEST_DATA_ROOT = Path(__file__).parent.parent / 'pyWavelet' / 'data'
 OIB_TEST_FILE = str(TEST_DATA_ROOT / 'sr' / 'Data_20160419_04_010.mat')
 AWI_TEST_FILE = str(TEST_DATA_ROOT / 'awi' / 'Data_20170410_01_006.mat')
 
+# fixtures are basically instantiated objects for testing purposes
 # these are the testing instances that will be used for further test functions
 @pytest.fixture
 def oib_full():
@@ -25,11 +26,27 @@ def awi_full():
 def awi_meta():
     return AWI(AWI_TEST_FILE, l_case='meta')
 
-def test_awi_full_thing(oib_full):
-    assert type(str(awi_full)) == str
+def test_awi_str_repr(awi_full):
+    expected = f'AWI Datafile: {Path(AWI_TEST_FILE).name}'
+    assert str(awi_full) == expected
 
-def test_snowradar_as_dict(oib_full, oib_meta):
-    print(oib_meta.time_gps)
+def test_oib_str_repr(oib_full):
+    expected = f'OIB Datafile: {Path(OIB_TEST_FILE).name}'
+    assert str(oib_full) == expected
+
+def test_awi_as_dict(awi_full, awi_meta):
+    assert awi_full.as_dict() == None
+    '''
+    assert awi_meta.as_dict() == {
+        'fname': awi_meta.file_name,
+        'fpath': awi_meta.file_path,
+        'tstart': awi_meta.time_utc[0],
+        'tend': awi_meta.time_utc[-1],
+        'poly': awi_meta.poly
+    }
+    '''
+
+def test_oib_as_dict(oib_full, oib_meta):
     assert oib_full.as_dict() == None
     '''
     assert oib_meta.as_dict() == {
