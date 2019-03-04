ff086272e85d667758c1e1563cf4ceaf8d61f6c3
Mike Brady <mike.brady@canada.ca>
2019-03-04 15:19:06 -0500

    Add hi-res landmask, update setup.py to accomodate Update processing.geo_filter() to use new landmask Update docstrings

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     long_description_content_type='text/markdown',
     url='path_to_github_repo',
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: canada_open?',
