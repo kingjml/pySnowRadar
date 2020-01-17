5dc1d305256c8e14195f1518f4a2809078da07fb
Mike Brady <mike.brady@canada.ca>
2020-01-17 11:27:36 -0500

    Flesh out setup.py - Add a minimal install_requires that doesn't include jupyter or pytest - remove unused numpy import in layerpicking.py - add a more precise "dev" environment yml file (can be removed later)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open('README.md', 'r') as readme:
     long_description = readme.read()
@@ -8,15 +8,47 @@ setup(
     version='0.0.1',
     author='Climate Research Division',
     author_email='',
-    description='A package for working with ',
+    description='A Python package to process data from CRESIS SnowRadar systems.',
+    license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='path_to_github_repo',
+    url='https://github.com/kingjml/pySnowRadar',
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: canada_open?',
-        'Operating System :: OS Independent',
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering'
     ],
+    python_requires='>=3,<3.8',
+    install_requires=[
+        'gdal>=2.3.3',
+        'h5py>=2.9',
+        'matplotlib>=3.1',
+        'numpy>=1.16',
+        'pywavelets>=1',
+        'shapely>=1.6.4',
+        'scipy>=1.2.1',
+        'pandas>=0.24.2',
+        'geopandas>=0.4.1',
+        'pyproj>=1.9.6',
+    ],
+    extras_require={
+        'test': ['pytest', 'coverage'],
+        'notebooks': ['jupyter']
+    },
+    keywords='snowradar',
+    project_urls={
+        'Documentation': '',
+        'Source': 'https://github.com/kingjml/pySnowRadar',
+        'Tracker': 'https://github.com/kingjml/pySnowRadar/issues',
+    },
 )
